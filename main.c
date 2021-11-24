443d9e2e76fcafcb487acf57795f8887a62848c0
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2021-11-24 00:30:59 -0300

    comprando arquivos

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 #include<time.h>
 
 int main(){
-    Lista * lista = create_lista();
+    Lista * lista = create_lista(), * lista2 = create_lista();
     for(int i=0;i<100;i++){
-        lista = push_order(lista,rand()%10000);
+        int val = rand()%10000;
+        lista = push_order(lista,val);
+        lista2 = push_no(lista2,val);
     }
     printf("tamanho: %d\n", lista->length);
     for(int i=0;i<100;i++){
-        printf("%d\n",lista_pop(lista));
+        printf("%d:%d\n",lista_pop(lista),lista_pop(lista2));
     }
     free_lista(lista);
+    free_lista(lista2);
     return 0;
 }
