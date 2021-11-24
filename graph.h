7a1e13dfdd932c264b154385c6b777d457643e0c
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2021-11-24 01:06:40 -0300

    stating to work on graph file

--- graph.h
+++ graph.h
@@ -16,8 +16,36 @@
 
 
 typedef struct Node{
-     Lista lista_adj ;
+     Lista * lista_adj ;
      int valor;
+     int visitado;
 }Node;
+
+Node* push_adj(Node * node, int valor){
+    if(node->lista_adj == NULL){
+        node->lista_adj = create_lista();
+    }
+    node->lista_adj = push_order(node->lista_adj,valor);
+    return node;
+}
+
+int size_adj_lista(Node * node){
+    if(node->lista_adj== NULL){
+        return 0;
+    }
+    return node->lista_adj->length;
+}
+
+void visitar(Node *node){
+    node->visitado=1;
+}
+
+int vistado(Node * node){
+    return node->visitado;
+}
+
+void limpar_visita(Node * node){
+    node->visitado=0;
+}
 
 #endif
